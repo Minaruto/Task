61c9fc452ed903203e61253c7d114fe8adc15637
MyroneGonsalves <myrone.gonsalves@outlook.com>
2022-08-13 21:49:30 +0400

    Added json support for express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const fs = require()
+const fs = require("fs/promises");
+const express = require("express");
+const cors = require("cors");
+const _ = require("lodash");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => console.log("API Server is running..."));
+
+app.post("/api/search", (req, res) => {
+    s
+});
