9080f49da78ef0b8cf100074561260a010b90b3e
MyroneGonsalves <myrone.gonsalves@outlook.com>
2022-08-21 21:23:49 +0400

    Added app.js and included fetching logic when api endpoint is called, in the index.js file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 const fs = require("fs/promises");
 const express = require("express");
 const cors = require("cors");
@@ -10,5 +12,19 @@ app.use(express.json());
 app.listen(3000, () => console.log("API Server is running..."));
 
 app.post("/api/search", (req, res) => {
-    s
+    const { type, text } = req.body;
+    let url = 'https://api.github.com/users/' + text;
+    fetch(url).then(res=>res.json()).then(data=>{
+        if(data.message){
+            console.log('User profile not found')
+        }
+        else{
+            let username = data.name;
+            let pfp = data.avatar_url;
+        }
+        res.send({
+            username,
+            pfp
+        });
+    });
 });
