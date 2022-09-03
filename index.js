63453cec57ffd9e3366cf7b9fbf78c11be05a129
MyroneGonsalves <myrone.gonsalves@outlook.com>
2022-09-03 21:53:50 +0400

    Added files

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import fetch from 'node-fetch';
-
 const fs = require("fs/promises");
 const express = require("express");
 const cors = require("cors");
 const _ = require("lodash");
 
 const app = express();
+
+const fetch = (...args) =>
+	import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 app.use(express.json());
 
@@ -14,17 +15,5 @@ app.listen(3000, () => console.log("API Server is running..."));
 app.post("/api/search", (req, res) => {
     const { type, text } = req.body;
     let url = 'https://api.github.com/users/' + text;
-    fetch(url).then(res=>res.json()).then(data=>{
-        if(data.message){
-            console.log('User profile not found')
-        }
-        else{
-            let username = data.name;
-            let pfp = data.avatar_url;
-        }
-        res.send({
-            username,
-            pfp
-        });
-    });
+    fetch(url).then(res => res.json()).then(data => console.log(data));
 });
