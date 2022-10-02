a0d34340642e3f6852a55c6162dbf919ccb0e77c
MyroneGonsalves <myrone.gonsalves@outlook.com>
2022-10-02 16:39:20 +0400

    Added Fliter logic

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const _ = require("lodash");
 
 const app = express();
 
+//allows us to use fetch inside the endpoint
 const fetch = (...args) =>
 	import('node-fetch').then(({default: fetch}) => fetch(...args));
 
@@ -15,5 +16,18 @@ app.listen(3000, () => console.log("API Server is running..."));
 app.post("/api/search", (req, res) => {
     const { type, text } = req.body.text;
     let url = 'https://api.github.com/users?q=' + text + 'in:user';
-    fetch(url).then(res => res.json()).then(data => console.log(data));
-});
+
+    //fetching list of users from Github API
+    fetch(url)
+    .then(res => res.json())
+    .then(body => {
+        let arr = [];
+        arr = body.map((user) => {
+            const {login, avatar_url} = user;
+            console.log({login, avatar_url});}
+            //arr = { login: user.login, avatar_url: user.avatar_url };
+        )});
+        //console.log(arr);
+        //(user) => user.login, user.avatar_url
+        });
+//});
